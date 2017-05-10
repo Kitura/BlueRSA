1c6aa5eb1b1161df4d2072fef503403169d2f0d5
rolivieri <roliv@us.ibm.com>
2017-05-10 14:42:44 -0500

    Updating code so that it can compile on both platforms, macOS and linux.

--- Sources/CryptorRSAUtilities.swift
+++ Sources/CryptorRSAUtilities.swift
@@ -28,9 +28,9 @@ import Foundation
 ///
 @available(macOS 10.12, iOS 10.0, *)
 public extension CryptorRSA {
-	
+
 	#if !os(Linux)
-	
+
 	///
 	/// Create a key from key data.
 	///
@@ -41,27 +41,27 @@ public extension CryptorRSA {
 	///	- Returns:				`SecKey` representation of the key.
 	///
 	static func createKey(from keyData: Data, type: CryptorRSA.RSAKey.KeyType) throws ->  SecKey {
-		
+
 		var keyData = keyData
-		
+
 		let keyClass = type == .publicType ? kSecAttrKeyClassPublic : kSecAttrKeyClassPrivate
-		
+
 		let sizeInBits = keyData.count * MemoryLayout<UInt8>.size
 		let keyDict: [CFString: Any] = [
 			kSecAttrKeyType: kSecAttrKeyTypeRSA,
 			kSecAttrKeyClass: keyClass,
 			kSecAttrKeySizeInBits: NSNumber(value: sizeInBits)
 		]
-		
+
 		guard let key = SecKeyCreateWithData(keyData as CFData, keyDict as CFDictionary, nil) else {
-			
+
 			throw Error(code: ERR_ADD_KEY, reason: "Couldn't create key reference from key data")
 		}
-		
+
 		return key
-		
+
 	}
-	
+
 	#endif
 
 	///
@@ -73,23 +73,23 @@ public extension CryptorRSA {
 	/// - Returns:				Base64 encoded `String` containing the data.
 	///
 	static func base64String(for pemString: String) throws -> String {
-		
+
 		// Filter looking for new lines...
 		var lines = pemString.components(separatedBy: "\n").filter { line in
 			return !line.hasPrefix(CryptorRSA.GENERIC_BEGIN_MARKER) && !line.hasPrefix(CryptorRSA.GENERIC_END_MARKER)
 		}
-		
+
 		// No lines, no data...
 		guard lines.count != 0 else {
 			throw Error(code: ERR_BASE64_PEM_DATA, reason: "Couldn't get data from PEM key: no data available after stripping headers.")
 		}
-		
+
 		// Strip off any carriage returns...
 		lines = lines.map { $0.replacingOccurrences(of: "\r", with: "") }
-		
+
 		return lines.joined(separator: "")
 	}
-	
+
 	///
 	/// This function strips the x509 from a provided ASN.1 DER public key. If the key doesn't contain a header,
 	///	the DER data is returned as is.
@@ -100,67 +100,68 @@ public extension CryptorRSA {
 	/// - Returns:					`Data` containing the public with header (if present) removed.
 	///
 	static func stripPublicKeyHeader(for keyData: Data) throws -> Data {
-		
+
 		let count = keyData.count / MemoryLayout<CUnsignedChar>.size
-		
+
 		guard count > 0 else {
-			
+
 			throw Error(code: ERR_STRIP_PK_HEADER, reason: "Provided public key is empty")
 		}
-		
-		var byteArray = [UInt8](repeating: 0, count: count)
-		(keyData as NSData).getBytes(&byteArray, length: keyData.count)
-		
+
+		//var byteArray = [UInt8](repeating: 0, count: count)
+		//(keyData as NSData).getBytes(&byteArray, length: keyData.count)
+		var byteArray = [UInt8](keyData)
+
 		var index = 0
 		guard byteArray[index] == 0x30 else {
-			
+
 			throw Error(code: ERR_STRIP_PK_HEADER, reason: "Provided key doesn't have a valid ASN.1 structure (first byte should be 0x30 == SEQUENCE)")
 		}
-		
+
 		index += 1
 		if byteArray[index] > 0x80 {
 			index += Int(byteArray[index]) - 0x80 + 1
 		} else {
 			index += 1
 		}
-		
+
 		// If current byte marks an integer (0x02), it means the key doesn't have a X509 header and just
 		// contains its modulo & public exponent. In this case, we can just return the provided DER data as is.
 		if Int(byteArray[index]) == 0x02 {
 			return keyData
 		}
-		
+
 		// Now that we've excluded the possibility of headerless key, we're looking for a valid X509 header sequence.
 		// It should look like this:
 		// 0x30, 0x0d, 0x06, 0x09, 0x2a, 0x86, 0x48, 0x86, 0xf7, 0x0d, 0x01, 0x01, 0x01, 0x05, 0x00
 		guard Int(byteArray[index]) == 0x30 else {
-			
+
 			throw Error(code: ERR_STRIP_PK_HEADER, reason: "Provided key doesn't have a valid X509 header")
 		}
-		
+
 		index += 15
 		if byteArray[index] != 0x03 {
-			
+
 			throw Error(code: ERR_STRIP_PK_HEADER, reason: "Invalid byte at index \(index - 1) (\(byteArray[index - 1])) for public key header")
 		}
-		
+
 		index += 1
 		if byteArray[index] > 0x80 {
 			index += Int(byteArray[index]) - 0x80 + 1
 		} else {
 			index += 1
 		}
-		
+
 		guard byteArray[index] == 0 else {
-			
+
 			throw Error(code: ERR_STRIP_PK_HEADER, reason: "Invalid byte at index \(index - 1) (\(byteArray[index - 1])) for public key header")
 		}
-		
+
 		index += 1
-		
+
 		let strippedKeyBytes = [UInt8](byteArray[index...keyData.count - 1])
 		let data = Data(bytes: UnsafePointer<UInt8>(strippedKeyBytes), count: keyData.count - index)
-		
+
 		return data
 	}
 }
@@ -170,21 +171,21 @@ public extension CryptorRSA {
 #if !os(Linux)
 
 	// MARK: -- CFString Extension for Hashing
-	
+
 	///
 	/// Extension to CFString to make it hashable.
 	///
 	extension CFString: Hashable {
-		
+
 		/// Return the hash value of a CFString
 		public var hashValue: Int {
 			return (self as String).hashValue
 		}
-		
+
 		/// Comparison of CFStrings
 		static public func == (lhs: CFString, rhs: CFString) -> Bool {
 			return lhs as String == rhs as String
 		}
 	}
-	
+
 #endif
