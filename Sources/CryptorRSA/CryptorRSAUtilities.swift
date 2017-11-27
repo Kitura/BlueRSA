c22ab477cf9510f4f319f6edf34708c29651a9b5
Gelareh Taban <gtaban@us.ibm.com>
2017-11-27 15:42:38 -0600

    Fix warnings about deprecated characters since in Swift4 String is a collection of characters (again).

--- Sources/CryptorRSA/CryptorRSAUtilities.swift
+++ Sources/CryptorRSA/CryptorRSAUtilities.swift
@@ -305,7 +305,7 @@ extension String {
 		collectedCharacters.reserveCapacity(length)
 		var count = 0
 		
-		for character in self.characters {
+		for character in self {
 			collectedCharacters.append(character)
 			count += 1
 			if (count == length) {
