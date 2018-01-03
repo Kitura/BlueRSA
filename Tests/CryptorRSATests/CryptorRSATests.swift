b7e01b8d4a112734f18648e9b8df3018208cd6c2
Gelareh Taban <gtaban@us.ibm.com>
2018-01-03 15:39:36 -0600

    Add in the remaining changes for encryption support and enable the encryption tests on Linux.

--- Tests/CryptorRSATests/CryptorRSATests.swift
+++ Tests/CryptorRSATests/CryptorRSATests.swift
@@ -507,9 +507,9 @@ class CryptorRSATests: XCTestCase {
 //			("test_private_initWithPEMStringHeaderless", test_private_initWithPEMStringHeaderless),
             ("test_private_initWithPEMName", test_private_initWithPEMName),
             ("test_private_initWithDERName", test_private_initWithDERName),
-//			("test_simpleEncryption", test_simpleEncryption),
-//			("test_longStringEncryption", test_longStringEncryption),
-//			("test_randomByteEncryption", test_randomByteEncryption),
+            ("test_simpleEncryption", test_simpleEncryption),
+            ("test_longStringEncryption", test_longStringEncryption),
+            ("test_randomByteEncryption", test_randomByteEncryption),
 			("test_signVerifyAllDigestTypes", test_signVerifyAllDigestTypes),
 			("test_signVerifyBase64", test_signVerifyBase64),
         ]
