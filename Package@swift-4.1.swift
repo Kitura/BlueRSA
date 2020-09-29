efc4b81383dff687bdf80e5701c422f45f08098d
Danny Sung <danny@dannysung.com>
2020-09-28 23:00:58 -0700

    Updated dependencies to community versions.

--- Package@swift-4.1.swift
+++ Package@swift-4.1.swift
@@ -27,7 +27,7 @@ var targetDependencies: [Target.Dependency] = []
 #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
 	
     let CryptoLibUrl = "https://github.com/Kitura/CommonCrypto.git"
-    let CryptoLibVersion: Package.Dependency.Requirement = .upToNextMajor(from: "1.0.0")
+    let CryptoLibVersion: Package.Dependency.Requirement = .upToNextMajor(from: "1.0.200")
 	
 #elseif os(Linux)
 	
